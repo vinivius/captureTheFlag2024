0abd9d20ac7901d02616636df7276c20d0d98cb1
vinivius <vinivius@gmail.com>
2024-09-15 12:48:54 -0400

    Update integridade1script.js

--- integridade1script.js
+++ integridade1script.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 // Serve the flag.txt file when the root route is accessed
 app.get('/', (req, res) => {
