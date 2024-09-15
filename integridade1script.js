5e2eea3bfca0a858898a925bc06044bc29fb04e7
vinivius <vinivius@gmail.com>
2024-09-15 12:34:56 -0400

    Create integridade1script.js

---
+++ integridade1script.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// Serve the flag.txt file when the root route is accessed
+app.get('/', (req, res) => {
+    const filePath = path.join(__dirname, 'flag.txt');
+    res.sendFile(filePath, (err) => {
+        if (err) {
+            console.error('Error sending file:', err);
+            res.status(500).send('Error serving file.');
+        }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
