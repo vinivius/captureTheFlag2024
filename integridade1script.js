55f4f45ea8ec62e84a1045d8f64784feb22b9b34
vinivius <vinivius@gmail.com>
2024-09-15 12:59:11 -0400

    Update integridade1script.js

--- integridade1script.js
+++ integridade1script.js
@@ -1,10 +1,23 @@
 const fs = require('fs');
+const http = require('http');
 
-// Read the file asynchronously
-fs.readFile('flag.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error('Error reading the file:', err);
-        return;
-    }
-    console.log('File contents:', data);
+// Path to the file
+const filePath = 'flag.txt';
+
+// Create a simple HTTP server
+http.createServer((req, res) => {
+    // Read the contents of the flag.txt file
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Error reading file');
+            return;
+        }
+
+        // Send the file contents as the HTTP response
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(data);
+    });
+}).listen(8080, () => {
+    console.log('Server running at http://localhost:8080/');
 });
